f9f9d3dac05c166c32c3088181e06761a573c395
jeongin <jeonginchoi00@gmail.com>
2025-11-12 17:17:39 +0900

    [최정인] UIManager, Template 코드 추가

--- Assets/02_Scripts/Global/GlobalEnums.cs
+++ Assets/02_Scripts/Global/GlobalEnums.cs
@@ -2,5 +2,33 @@ using UnityEngine;
 
 namespace Globals
 {
+    #region UI
+    public enum PageType
+    {
+        NONE = 0,
 
+        // Title
+
+        // Main
+
+        // Game
+
+        // BossGame
+
+    }
+
+    public enum PopupType
+    {
+        NONE = 0,
+
+    }
+    #endregion
+
+    #region Sound
+    public enum SoundType
+    {
+        NONE = 0,
+
+    }
+    #endregion
 }
