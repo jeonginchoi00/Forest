2775be26e6a52381b5ef6fd3b4b9f6f5b40eb386
jeongin <jeonginchoi00@gmail.com>
2025-11-12 18:34:48 +0900

    [최정인] 코드 추가

--- Assets/02_Scripts/Manager/SoundManager.cs
+++ Assets/02_Scripts/Manager/SoundManager.cs
@@ -2,5 +2,20 @@ using UnityEngine;
 
 public class SoundManager : MonoBehaviour
 {
+    private static SoundManager m_instance;
+    public static SoundManager GetInstance() => m_instance;
 
+    private void Awake()
+    {
+        if (m_instance == null)
+        {
+            m_instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
 }
--- Assets/02_Scripts/Manager/UserInfoManager.cs
+++ Assets/02_Scripts/Manager/UserInfoManager.cs
@@ -2,5 +2,19 @@ using UnityEngine;
 
 public class UserInfoManager : MonoBehaviour
 {
+    private static UserInfoManager m_instance;
+    public static UserInfoManager GetInstance() => m_instance;
 
+    private void Awake()
+    {
+        if (m_instance == null)
+        {
+            m_instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
 }
