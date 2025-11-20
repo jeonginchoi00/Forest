25cc01843f691840dc1eb7485213d8729dec5232
jeongin <jeonginchoi00@gmail.com>
2025-11-20 14:19:46 +0900

    [최정인] VFX 파일 추가, 이펙트 작업 중

--- Assets/02_Scripts/Item/CoinBox.cs
+++ Assets/02_Scripts/Item/CoinBox.cs
@@ -5,6 +5,7 @@ public class CoinBox : MonoBehaviour
 {
     [SerializeField] private GameObject m_openBoxPrefab;
     [SerializeField] private GameObject m_coinPrefab;
+    [SerializeField] private GameObject m_effectPrefab;
     private int m_hitCount = 0;
     private int m_maxCount = 3;
 
@@ -14,6 +15,9 @@ public class CoinBox : MonoBehaviour
 
         if (m_hitCount >= m_maxCount)
         {
+            Vector3 effectPos = transform.position + new Vector3(0f, 0.3f, 0f);
+            PoolManager.GetInstance().Get(m_effectPrefab, effectPos, Quaternion.identity);
+
             Instantiate(m_coinPrefab, transform.position, Quaternion.identity);
             Instantiate(m_openBoxPrefab, transform.position, Quaternion.identity);
 
